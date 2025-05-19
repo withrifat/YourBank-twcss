b9dc672bcd91a1cc7ba52fa92f41b835e83deabd
withrifat <mdrifathassain97880@gmail.com>
2025-05-20 01:36:57 +0600

    add index.html file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-
 // for dark mode code
-document.documentElement.classList.remove('dark');
+const htmlTag = document.getElementById('htmlTag');
 document.getElementById('switch').addEventListener('click', function () {
-  document.documentElement.classList.toggle('dark');
+  htmlTag.classList.toggle('dark');
+});
+
+// code for mobile menu
+document.addEventListener('DOMContentLoaded', function () {
+  const mobileMenuButton = document.getElementById('mobile-menu-button');
+  const closeMenuButton = document.getElementById('close-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  mobileMenuButton.addEventListener('click', function () {
+    mobileMenu.classList.remove('hidden');
+    mobileMenu.classList.add('flex');
+    document.body.style.overflow = 'hidden'; // Prevent scrolling when menu is open
+  });
+
+  closeMenuButton.addEventListener('click', function () {
+    mobileMenu.classList.remove('flex');
+    mobileMenu.classList.add('hidden');
+    document.body.style.overflow = ''; // Re-enable scrolling
+  });
 });
